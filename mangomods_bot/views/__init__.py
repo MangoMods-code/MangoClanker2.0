a0b14a352f6feff8063d07b3ec718b280f12c5ce
MangoMods-code <nicholasbeiling29@gmail.com>
2026-06-25 21:09:57 -0400

    Expand __all__ exports with new ticket views and modals
    Added new ticket-related views and modals to __all__ export.

--- mangomods_bot/views/__init__.py
+++ mangomods_bot/views/__init__.py
@@ -1,4 +1,17 @@
 from .ticket_panel import TicketPanelView
 from .ticket_actions import TicketActionsView
+from .ticket_rating import TicketRatingView
+from .ticket_priority import TicketPriorityView
+from .ticket_modals import PurchaseTicketModal, SupportTicketModal, GeneralTicketModal
+from .ticket_close_confirm import TicketCloseConfirmView
 
-__all__ = ["TicketPanelView", "TicketActionsView"]
+__all__ = [
+    "TicketPanelView",
+    "TicketActionsView",
+    "TicketRatingView",
+    "TicketPriorityView",
+    "PurchaseTicketModal",
+    "SupportTicketModal",
+    "GeneralTicketModal",
+    "TicketCloseConfirmView",
+]
